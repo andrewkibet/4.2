c9ceffc7ec7b234a1335557f02e664c7cdf051ec
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-08 11:17:19 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/a42/MainActivity.java
+++ app/src/main/java/com/example/a42/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.a42;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swiperefresh);
+        listView = findViewById(R.id.listview);
     }
 }
