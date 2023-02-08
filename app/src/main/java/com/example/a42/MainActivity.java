1d7e3ded82b8fb87dc61b1c9372e0588f2e7f5a3
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-08 11:26:45 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/a42/MainActivity.java
+++ app/src/main/java/com/example/a42/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.a42;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    class LoadAppinfoTask extends AsyncTask<Integer,Integer, List<Appinfo>>
+    {
+        @Override
+        protected List<Appinfo> doInBackground(Integer... integers) {
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
     }
 }
