c133d7c937ec6d628880c19f2ed22fec0909be8e
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-09 19:57:51 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/a42/MainActivity.java
+++ app/src/main/java/com/example/a42/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.a42;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                LoadAppinfoTask loadAppinfoTask = new LoadAppinfoTask();
+                loadAppinfoTask.execute(PackageManager.GET_META_DATA);
+
 
             }
         });
@@ -36,11 +42,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            swipeRefreshLayout.setRefreshing(true);
         }
         @Override
         protected List<Appinfo> doInBackground(Integer... integers) {
+
+            List<Appinfo> app= new ArrayList<>();
+            PackageManager packageManager = getPackageManager();
+            List<ApplicationInfo> infos = packageManager.getInstalledApplications(integers[0]);
+
+
+
             return null;
         }
 
+        @Override
+        protected void onPostExecute(List<Appinfo> appinfos) {
+            super.onPostExecute(appinfos);
+        }
     }
 }
