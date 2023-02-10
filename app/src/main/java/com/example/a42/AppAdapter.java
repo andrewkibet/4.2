f63fc84c30ad48c200e8a511e3dbd8c94369979d
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-10 12:27:40 +0300

    Update AppAdapter.java

--- app/src/main/java/com/example/a42/AppAdapter.java
+++ app/src/main/java/com/example/a42/AppAdapter.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public class AppAdapter extends ArrayAdapter<Appinfo> {
     public AppAdapter(@NonNull Context context, List<Appinfo> apps) {
-        super(context, R.layout.app_items);
+        super(context, R.layout.app_items,apps);
     }
 }
