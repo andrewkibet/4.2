11984a51e0bada18dc7576fcb67c15cee7c03143
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-10 12:24:38 +0300

    Update AppAdapter.java

--- app/src/main/java/com/example/a42/AppAdapter.java
+++ app/src/main/java/com/example/a42/AppAdapter.java
@@ -1,4 +1,12 @@
 package com.example.a42;
 
-public class AppAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+public class AppAdapter extends ArrayAdapter<Appinfo> {
+    public AppAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
 }
