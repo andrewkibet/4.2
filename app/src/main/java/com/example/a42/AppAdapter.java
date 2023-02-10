9fa4dd6c5d52da2ddc7ffa4773d66ed1c02d8014
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-10 12:34:49 +0300

    Update AppAdapter.java

--- app/src/main/java/com/example/a42/AppAdapter.java
+++ app/src/main/java/com/example/a42/AppAdapter.java
@@ -3,9 +3,12 @@ package com.example.a42;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -22,5 +25,11 @@ public class AppAdapter extends ArrayAdapter<Appinfo> {
         packageManager = context.getPackageManager();
         this.apps = apps;
 
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
