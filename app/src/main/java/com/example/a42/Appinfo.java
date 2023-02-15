eba53f91f907561dbbace8a156c4be84615e74d7
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-15 10:42:42 +0300

    Update Appinfo.java

--- app/src/main/java/com/example/a42/Appinfo.java
+++ app/src/main/java/com/example/a42/Appinfo.java
@@ -8,5 +8,5 @@ import java.util.List;
 public class Appinfo {
     public ApplicationInfo info;
     public String label;
-    public static List<String> permissions = new ArrayList<>();
+    public List<String> permissions = new ArrayList<>();
 }
