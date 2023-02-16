1bbebe4339111eb130ac2f1cc4c80227490fad37
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-16 12:17:44 +0300

    Update PermissionM_Activity.java

--- app/src/main/java/com/example/a42/PermissionM_Activity.java
+++ app/src/main/java/com/example/a42/PermissionM_Activity.java
@@ -1,14 +1,22 @@
 package com.example.a42;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class PermissionM_Activity extends AppCompatActivity {
+    SwipeRefreshLayout swipeRefreshLayout;
+    ListView listView ;
+    boolean allsystemapps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission_mactivity);
+        swipeRefreshLayout=findViewById(R.id.swiperefresh);
+        listView =  findViewById(R.id.listview);
+
     }
 }
