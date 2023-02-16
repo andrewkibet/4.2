4e2e1361dfa75b80b06ce7f8b9226d2de1717d64
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-16 12:34:43 +0300

    Update Appinfo.java

--- app/src/main/java/com/example/a42/Appinfo.java
+++ app/src/main/java/com/example/a42/Appinfo.java
@@ -6,13 +6,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Appinfo {
-    public ApplicationInfo info;
-    public String label;
-    public List<String> permissions = new ArrayList<>();
+    ApplicationInfo info;
+    String label;
+    List<String> permissions = new ArrayList<>();
+    boolean hasCameraPermission;
+    boolean hasAudioPermission;
 
-    public void setInfo(ApplicationInfo info) {
+    public ApplicationInfo getInfo() {
+        return info;
     }
 
-    public void setLabel(String toString) {
+    public void setInfo(ApplicationInfo info) {
+        this.info = info;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<String> permissions) {
+        this.permissions = permissions;
+    }
+
+    public boolean hasCameraPermission() {
+        return hasCameraPermission;
+    }
+
+    public void setHasCameraPermission(boolean hasCameraPermission) {
+        this.hasCameraPermission = hasCameraPermission;
+    }
+
+    public boolean hasAudioPermission() {
+        return hasAudioPermission;
+    }
+
+    public void setHasAudioPermission(boolean hasAudioPermission) {
+        this.hasAudioPermission = hasAudioPermission;
     }
 }
