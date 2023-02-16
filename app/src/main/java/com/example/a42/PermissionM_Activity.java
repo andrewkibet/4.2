ae9bfd4a5138e5938c88f0575741b779c6ee89b9
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-16 12:20:57 +0300

    Update PermissionM_Activity.java

--- app/src/main/java/com/example/a42/PermissionM_Activity.java
+++ app/src/main/java/com/example/a42/PermissionM_Activity.java
@@ -3,8 +3,17 @@ package com.example.a42;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PermissionM_Activity extends AppCompatActivity {
     SwipeRefreshLayout swipeRefreshLayout;
@@ -17,6 +26,70 @@ public class PermissionM_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_permission_mactivity);
         swipeRefreshLayout=findViewById(R.id.swiperefresh);
         listView =  findViewById(R.id.listview);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                refreshIt();
+
+            }
+        });
+
 
     }
-}
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MainActivity.LoadAppinfoTask loadAppinfoTask = new MainActivity.LoadAppinfoTask();
+        loadAppinfoTask.execute(PackageManager.GET_META_DATA);
+    }
+
+    private void refreshIt() {
+        MainActivity.LoadAppinfoTask loadAppinfoTask = new MainActivity.LoadAppinfoTask();
+        loadAppinfoTask.execute(PackageManager.GET_META_DATA);
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    class LoadAppinfoTask extends AsyncTask<Integer,Integer, List<Appinfo>>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            swipeRefreshLayout.setRefreshing(true);
+        }
+
+        @Override
+        protected List<Appinfo> doInBackground(Integer... integers) {
+            List<Appinfo> apps= new ArrayList<>();
+            PackageManager packageManager = getPackageManager();
+            List<ApplicationInfo> infos = packageManager.getInstalledApplications(integers[0]);
+
+            for (ApplicationInfo info : infos) {
+                if (allsystemapps && (info.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                    continue;
+                }
+
+                Appinfo appinfo = new Appinfo();
+                appinfo.info = info;
+                appinfo.label = (String) info.loadLabel(packageManager);
+
+                try {
+                    PackageInfo packageInfo = packageManager.getPackageInfo(info.packageName, PackageManager.GET_PERMISSIONS);
+                    if (packageInfo.requestedPermissions != null) {
+                        appinfo.permissions.addAll(Arrays.asList(packageInfo.requestedPermissions));
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                apps.add(appinfo);
+            }
+
+            return apps;
+
+
+        }
