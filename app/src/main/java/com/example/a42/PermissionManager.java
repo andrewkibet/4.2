91e618c0ff54e2448eee327cc31e02e123a15649
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-16 10:25:59 +0300

    Update PermissionManager.java

--- app/src/main/java/com/example/a42/PermissionManager.java
+++ app/src/main/java/com/example/a42/PermissionManager.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class PermissionManager extends AppCompatActivity {
+public class PermissionManager extends AppCompatActivity implements PermissionManagerrr {
 
     boolean allsystemapps = true;
 
