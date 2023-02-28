0a2968ebb1ac61767f7e36a9394bb92c9307e6dd
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-28 16:47:09 +0300

    Update Notification.java

--- app/src/main/java/com/example/a42/Notification.java
+++ app/src/main/java/com/example/a42/Notification.java
@@ -3,6 +3,9 @@ package com.example.a42;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,6 +17,11 @@ public class Notification extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         TextView notifyText = findViewById(R.id.not);
+
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notificatio", "My Not", NotificationManager.IMPORTANCE_DEFAULT);
+        }
+
         notifyText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
