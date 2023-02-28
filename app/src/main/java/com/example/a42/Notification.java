d0cfb2ce37f43d1a799332362aab9bc97125112e
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-28 16:34:38 +0300

    Update Notification.java

--- app/src/main/java/com/example/a42/Notification.java
+++ app/src/main/java/com/example/a42/Notification.java
@@ -2,6 +2,8 @@ package com.example.a42;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Notification extends AppCompatActivity {
 
@@ -9,7 +11,14 @@ public class Notification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        TextView notifyText = findViewById(R.id.not);
+        notifyText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 
-    
+
 }
