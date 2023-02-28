63dd64694508dbe494a1f4b6a3805b09fc542435
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-02-28 18:13:08 +0300

    Update Notification.java

--- app/src/main/java/com/example/a42/Notification.java
+++ app/src/main/java/com/example/a42/Notification.java
@@ -6,8 +6,10 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -30,20 +32,34 @@ public class Notification extends AppCompatActivity {
         notifyText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(Notification.this,"channel Id");                        builder.setContentTitle("Camera Accessed");
-                        builder.setContentText("Be Careful");
-                        builder.setContentTitle("Camera");
-                       builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-                        //.setContentIntent(pendingIntent)
-                       builder.setSmallIcon(R.drawable.ic_camera);
-                      builder.setAutoCancel(true);
+                // Check if notification channel is null
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+                    NotificationChannel channel = new NotificationChannel("My Notificatio", "My Not", NotificationManager.IMPORTANCE_DEFAULT);
+                    NotificationManager manager = getSystemService(NotificationManager.class);
+                    if (manager != null) {
+                        manager.createNotificationChannel(channel);
+                    } else {
+                        Log.e("Notification", "NotificationManager is null");
+                        return;
+                    }
+                }
+
+                // Build the notification
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(Notification.this,"My Notificatio")
+                        .setContentTitle("Camera Accessed")
+                        .setContentText("Be Careful")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setSmallIcon(R.drawable.ic_camera)
+                        .setAutoCancel(true);
+
+                // Show the notification
                 NotificationManagerCompat managerCompat = NotificationManagerCompat.from(Notification.this);
-                managerCompat.notify(1,builder.build());
-
-
+                int notificationId = (int) System.currentTimeMillis();
+                managerCompat.notify(notificationId, builder.build());
+                Log.i("Notification", "Notification sent with ID " + notificationId);
             }
         });
-    }
 
 
-}
+
+    }}
