57dcae5a66ecdd1b64ccc7eb055b65c1055c4d4d
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-03-01 09:00:03 +0300

    Update CameraJobService.java

--- app/src/main/java/com/example/a42/CameraJobService.java
+++ app/src/main/java/com/example/a42/CameraJobService.java
@@ -1,4 +1,30 @@
 package com.example.a42;
 
-public class CameraJobService {
+import android.app.NotificationManager;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.util.Log;
+
+public class CameraJobService extends JobService {
+    private static final String TAG = "CameraJobService";
+    private NotificationManager notificationManager;
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Log.d(TAG, "Job started");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            notificationManager = getSystemService(NotificationManager.class);
+        }
+
+        // Implement camera usage monitoring logic here
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        Log.d(TAG, "Job stopped");
+        return true;
+    }
 }
