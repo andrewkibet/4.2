3ae8e126407a4939faa7a41e218fc641be5b38a2
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-03-01 09:07:59 +0300

    Update CameraUsageActivity.java

--- app/src/main/java/com/example/a42/CameraUsageActivity.java
+++ app/src/main/java/com/example/a42/CameraUsageActivity.java
@@ -1,3 +1,17 @@
  package com.example.a42;
 
+ import android.os.Bundle;
+
+ import androidx.appcompat.app.AppCompatActivity;
+
+ public class CameraUsageActivity extends AppCompatActivity {
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_camera_usage);
+
+         
+}
+ }
 
