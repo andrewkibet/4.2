ec66b8fe7c81642aec5943ba52128306279b35a4
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2023-03-02 11:07:38 +0300

    Update AudioBroadcastReceiver.java

--- app/src/main/java/com/example/a42/AudioBroadcastReceiver.java
+++ app/src/main/java/com/example/a42/AudioBroadcastReceiver.java
@@ -1,4 +1,46 @@
 package com.example.a42;
 
-public class AudioBroadcastReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+
+public class AudioBroadcastReceiver extends BroadcastReceiver {
+    private AudioManager mAudioManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (action != null) {
+            if (action.equals(AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
+                // Pause the audio playback or take any necessary action when the audio is becoming noisy
+                // For example, pause the music player when headphones are unplugged
+                // mAudioManager.pausePlayback();
+            } else if (action.equals(AudioManager.ACTION_HEADSET_PLUG)) {
+                // Take any necessary action when a headset is plugged in or unplugged
+                // For example, pause the music player when headphones are unplugged
+                int state = intent.getIntExtra("state", -1);
+                if (state == 0) {
+                    // Headset is unplugged
+                    // mAudioManager.pausePlayback();
+                } else if (state == 1) {
+                    // Headset is plugged in
+                    // mAudioManager.resumePlayback();
+                }
+            } else if (action.equals(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)) {
+                // Take any necessary action when the SCO (Bluetooth headset) audio state is updated
+                int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1);
+                if (state == AudioManager.SCO_AUDIO_STATE_CONNECTED) {
+                    // Bluetooth headset is connected
+                    // mAudioManager.setBluetoothScoOn(true);
+                    // mAudioManager.startBluetoothSco();
+                } else if (state == AudioManager.SCO_AUDIO_STATE_DISCONNECTED) {
+                    // Bluetooth headset is disconnected
+                    // mAudioManager.setBluetoothScoOn(false);
+                    // mAudioManager.stopBluetoothSco();
+                }
+            }
+        }
+    }
 }
+
