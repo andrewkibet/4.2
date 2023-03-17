e291659d3190130fe32f127dcead8ff281deae92
victor2872 <victor.mutai@kabarak.ac.ke>
2023-03-17 05:47:17 +0300

    changes

--- app/src/main/java/com/example/a42/CameraActivity.java
+++ app/src/main/java/com/example/a42/CameraActivity.java
@@ -1,14 +1,34 @@
 package com.example.a42;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class CameraActivity extends AppCompatActivity {
+
+
+    private static final String TAG = CameraActivity.class.getSimpleName();
+
+    private CameraManager mCameraManager;
+    private CameraManager.AvailabilityCallback mCameraCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-    }
+ }
 }
