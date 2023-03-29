6baf454524ba9bdc1ac9293b103a99ada08294c9
victor2872 <victor.mutai@kabarak.ac.ke>
2023-03-29 16:29:10 +0300

    Update HomePage.java

--- app/src/main/java/com/example/a42/HomePage.java
+++ app/src/main/java/com/example/a42/HomePage.java
@@ -2,14 +2,33 @@ package com.example.a42;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+    private Button bn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
        // getSupportActionBar().setTitle("Home");
+
+        bn = (Button) findViewById(R.id.moncamera);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openNewActivity();
+               }
+        });
+    }
+
+    private void openNewActivity() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+
     }
 }
