388bf3adf59ac71a6e7b62d683a4205dc6e5caff
victor2872 <victor.mutai@kabarak.ac.ke>
2023-03-29 16:57:28 +0300

    Update HomePage.java

--- app/src/main/java/com/example/a42/HomePage.java
+++ app/src/main/java/com/example/a42/HomePage.java
@@ -2,14 +2,16 @@ package com.example.a42;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
-    private Button bn;
+    private Button bn,allapps,pm;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,9 @@ public class HomePage extends AppCompatActivity {
        // getSupportActionBar().setTitle("Home");
 
         bn = (Button) findViewById(R.id.moncamera);
+        allapps=(Button) findViewById(R.id.bnall);
+        pm =(Button) findViewById(R.id.bnpm);
+
         bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
